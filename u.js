5d11986f60844340b723af46c36308835e4ad6f3
teammayhem <kaioramos240@gmail.com>
2026-05-11 20:53:08 -0300

    Update u.js

--- u.js
+++ u.js
@@ -1,18 +1,17 @@
 async function loadProfile() {
 
-  // PEGA O USERNAME DA URL
-  // exemplo:
-  // /krpris -> rewrite -> /u.html?user=krpris
-
   const params =
-  new URLSearchParams(window.location.search);
+  new URLSearchParams(
+    window.location.search
+  );
 
   const username =
   params.get("user");
 
-  console.log("USERNAME URL:", username);
-
-  console.log("USERNAME:", username);
+  console.log(
+    "USERNAME URL:",
+    username
+  );
 
   if (!username) {
 
@@ -23,8 +22,6 @@ async function loadProfile() {
 
   }
 
-  // BUSCA NO SUPABASE
-
   const { data, error } =
 
   await supabaseClient
@@ -33,8 +30,8 @@ async function loadProfile() {
   .eq("username", username)
   .single();
 
-  console.log(data);
-  console.log(error);
+  console.log("DATA:", data);
+  console.log("ERROR:", error);
 
   if (error || !data) {
 
@@ -45,49 +42,52 @@ async function loadProfile() {
 
   }
 
-  // TÍTULO
-
   document.title =
   `@${data.username}`;
 
-  // TEXTO
+  document.getElementById(
+    "username"
+  ).innerText =
+  data.display_name ||
+  data.username;
 
-  document.getElementById("username").innerText =
-  data.display_name || data.username;
-
-  document.getElementById("bio").innerText =
+  document.getElementById(
+    "bio"
+  ).innerText =
   data.bio || "";
 
-  // BALÃO
-
   const balao =
-  document.getElementById("balao");
+  document.getElementById(
+    "balao"
+  );
 
   if (
     !data.balao ||
     data.balao.trim() === ""
   ) {
 
-    balao.style.display = "none";
+    balao.style.display =
+    "none";
 
   } else {
 
-    balao.style.display = "block";
+    balao.style.display =
+    "block";
 
     balao.innerText =
     data.balao;
 
   }
 
-  // IMAGENS
-
-  document.getElementById("avatar").src =
+  document.getElementById(
+    "avatar"
+  ).src =
   data.avatar_url || "";
 
-  document.getElementById("banner").style.backgroundImage =
+  document.getElementById(
+    "banner"
+  ).style.backgroundImage =
   `url(${data.banner_url || ""})`;
-
-  // FUNDO
 
   if (data.background_url) {
 
@@ -108,17 +108,19 @@ async function loadProfile() {
 
   }
 
-  // REDES
-
   const socials =
-  document.getElementById("socials");
+  document.getElementById(
+    "socials"
+  );
 
   socials.innerHTML = "";
 
-  function addSocial(url, iconHTML) {
+  function addSocial(
+    url,
+    iconHTML
+  ) {
 
-    if (!url || url.trim() === "")
-    return;
+    if (!url) return;
 
     socials.innerHTML += `
       <a href="${url}" target="_blank">
