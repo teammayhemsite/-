d1374a0d7dbed83a83720fd769efaf510b2fe1ab
teammayhem <kaioramos240@gmail.com>
2026-05-25 23:04:16 -0300

    Update u.js

--- u.js
+++ u.js
@@ -259,49 +259,49 @@ if (
   addSocial(
     data.youtube_url,
     `<img src="https://www.riqueza.life/images/socials/youtube.png">`,
-    " Youtube", 
+    "ㅤYoutube", 
   );
 
   addSocial(
     data.instagram_url,
     `<img src="https://www.riqueza.life/images/socials/instagram.png">`,
-    " Instagram",     
+    "ㅤInstagram",     
   );
 
   addSocial(
   data.discord_url,
   `<img src="https://www.riqueza.life/images/socials/discord.png">`,
-  " Discord",  
+  "ㅤDiscord",  
 );
 
   addSocial(
     data.spotify_url,
     `<img src="https://www.riqueza.life/images/socials/spotify.png">`,
-    " Spotify", 
+    "ㅤSpotify", 
   );
 
   addSocial(
     data.tiktok_url,
     `<img src="https://www.riqueza.life/images/socials/tiktok.png">`,
-    " Tiktok", 
+    "ㅤTiktok", 
   );
 
   addSocial(
     data.whatsapp_url,
     `<img src="https://www.riqueza.life/images/socials/whatsapp.png">`,
-    " Whatsapp", 
+    "ㅤWhatsapp", 
   );
 
   addSocial(
     data.facebook_url,
     `<img src="https://www.riqueza.life/images/socials/twitch.png">`,
-    " Twitch", 
+    "ㅤTwitch", 
   );
 
   addSocial(
     data.twitter_url,
     `<img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/x.svg">`,
-    " Twiter", 
+    "ㅤTwiter", 
   );
 
   // =========================
